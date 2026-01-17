caf7a4d47382053e5670dc55a66ad19e5027f4d8
David Ho <hdavidh@amazon.com>
2026-01-17 00:36:59 -0800

    Revert "testing changes"
    This reverts commit 164402b4ea595660fdc2fbf92cd02be3a24ed0c7.

--- services/cloudwatch/src/it/java/software/amazon/awssdk/services/cloudwatch/CloudWatchIntegrationTest.java
+++ services/cloudwatch/src/it/java/software/amazon/awssdk/services/cloudwatch/CloudWatchIntegrationTest.java
@@ -96,14 +96,7 @@ public class CloudWatchIntegrationTest extends AwsIntegrationTestBase {
             DescribeAlarmsResponse describeResult = cloudwatch.describeAlarms(DescribeAlarmsRequest.builder().build());
             Collection<String> toDelete = new LinkedList<>();
             for (MetricAlarm alarm : describeResult.metricAlarms()) {
-                // TODO - null for some Catapult alarms
-                // Also other existing Catapult alarms we DON'T want to delete
-                String metricName = alarm.metricName();
-                if (metricName == null || metricName.startsWith("Catapult-Java2")) {
-                    continue;
-                }
-                //
-                if (metricName.startsWith(CloudWatchIntegrationTest.class.getName())) {
+                if (alarm.metricName().startsWith(CloudWatchIntegrationTest.class.getName())) {
                     toDelete.add(alarm.alarmName());
                 }
             }
